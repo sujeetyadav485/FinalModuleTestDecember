abfd264aa72d31e6958477480d623fd633056e06
Sujeet Yadav <99132296+sujeetyadav485@users.noreply.github.com>
2022-12-30 20:54:10 +0530

    SubmitModuleTestDecember

---
+++ script.js
@@ -0,0 +1,11 @@
+function showm(){
+    var firstName1 = document.getElementById('firstName1');
+    var lastName2 = document.getElementById('lastName2');
+    var emailId3 = document.getElementById('emailId3');
+
+    if (firstName1.value === '' | lastName2.value==='' | emailId3.value===''){
+        
+    }else {
+        document.getElementById('message').style.opacity = '1';
+    }
+}
